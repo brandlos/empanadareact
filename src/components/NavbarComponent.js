cd424a471ae3cc81de959f72d4576f2ed3f35592
Brandon <54649791+brandlos@users.noreply.github.com>
2021-08-04 12:05:20 -0400

    prettified navbar

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -3,12 +3,27 @@ import React, { Component } from "react";
 class Navbar extends Component {
   render() {
     return (
-      <header className="App-header">
-        <a href="/">EPB</a>
-        <a href="/locations">Locations</a>
-        <a href="/menus">Menus</a>
-        <a href="/bio">Bio</a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">EPB</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" href="/locations">Locations</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/menus">Menus</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/bio">Bio</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
